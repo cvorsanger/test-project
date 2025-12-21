b49225b021f93e09685a12be3d568975cd680ea3
Corey Vorsanger <cvorsanger@gmail.com>
2025-12-20 17:42:04 -0700

    Added Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { ComingSoon } from './coming-soon/coming-soon';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, ComingSoon],
+  imports: [Footer, ComingSoon, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true
